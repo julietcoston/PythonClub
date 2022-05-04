10105066a69195f0599bb16ca83b96980424b427
user <julietcoston@yahoo.com>
2022-05-04 11:09:36 -0700

    Assigment 5

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import TechType,TechProduct, Review
 # Register your models here.
+admin.site.register(TechType)
+admin.site.register(TechProduct)
+admin.site.register(Review)
--- pythonclub/settings.py
+++ pythonclub/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "club",
 ]
 
 MIDDLEWARE = [
@@ -71,14 +72,19 @@ WSGI_APPLICATION = 'pythonclub.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.0/ref/settings/#d
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER' : config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST' : config ('DB_HOST'),
+        'PORT' : config('DB_PORT'),
     }
-}
+}   
+
 
 
 # Password validation
